2df133fad6fd3a4f69a2df5cfa585a5589ddebb1
bkfleet1 <bkfleet1@gmail.com>
2022-01-17 14:03:58 -0600

    module 9.3.5 completed

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2);
+const pageHTML = generatePage(name, github);
 
-const [name, github] = profileDataArgs;
-
-fs.writeFile('index.html', generatePage(name, github), err => {
+fs.writeFile('./index.html', pageHTML, err => {
   if (err) throw err;
-  //why is err in () for if, but then not after throw?
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
