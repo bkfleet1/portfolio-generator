0b8aeebe5bffd3d48f399fd84eaaa880e7f984fc
bkfleet1 <bkfleet1@gmail.com>
2022-01-18 14:03:58 -0600

    lesson 9.5.5 generate-site.js added with writeFile and CopyFile functions

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
 const generatePage = require('./src/page-template');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-//   console.log('Portfolio complete! Check out index.html to see the output!');
-// });
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -68,11 +60,11 @@ Add a New Project
 `);
 
   // If there's no 'projects' array property, create one
-if (!portfolioData.projects) {
-  portfolioData.projects = [];
-}
+  if (!portfolioData.projects) {
+    portfolioData.projects = [];
+  }
 
-return inquirer.prompt([
+  return inquirer.prompt([
     {
       type: 'input',
       name: 'name',
@@ -132,26 +124,35 @@ return inquirer.prompt([
     }
   ])
 
-// The push() method adds new items to the end of an array.
-// The push() method changes the length of the array.
-// The push() method returns the new length.
+    // The push() method adds new items to the end of an array.
+    // The push() method changes the length of the array.
+    // The push() method returns the new length.
 
-  .then(projectData => {
-    portfolioData.projects.push(projectData);
-    if (projectData.confirmAddProject) {
-      return promptProject(portfolioData);
-    } else {
-      return portfolioData;
-    }
-  });
+    .then(projectData => {
+      portfolioData.projects.push(projectData);
+      if (projectData.confirmAddProject) {
+        return promptProject(portfolioData);
+      } else {
+        return portfolioData;
+      }
+    });
 };
 
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile('./index.html', pageHTML, err => {
-      if (err) throw new Error(err);
-      console.log('Page created! Check out index.html in this directory to see it!');
-    });
+    return generatePage(portfolioData);
+  })
+  .then(pageHTML => {
+    return writeFile(pageHTML);
+  })
+  .then(writeFileResponse => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then(copyFileResponse => {
+    console.log(copyFileResponse);
+  })
+  .catch(err => {
+    console.log(err);
   });
